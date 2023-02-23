3e6e077c933a0ce078b32ff0084c1f7bbed998ab
Juan Camilo <caamilo9517@gmail.com>
2023-02-23 14:05:08 -0500

    Data del menú lista, sólo faltan agregar las imágenes correctas

---
+++ js/dataMenu.js
@@ -0,0 +1,70 @@
+//Data de las comidas que tenemos.
+const data=[
+    {
+        id:1,
+        title:'Hambuegesa Mexicana',
+        img:'https://www.shutterstock.com/image-photo/beef-burgers-on-wooden-plate-260nw-1668890842.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Hamburguesa auténtica mexicana con los ingredientes frescos y de mejor calidad',
+        detailedDescription:'Hamburguesa con 150 gramos de res, lechuga, tomate, queso y jalapeños. Como salsa principal el guacamole con reseta propia de la casa. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:25000
+    },
+    {
+        id:2,
+        title:'Hambuegesa Tradicional',
+        img:'https://www.shutterstock.com/image-photo/beef-burgers-on-wooden-plate-260nw-1668890842.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Hamburguesa tradicional con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        detailedDescription:'Hamburguesa con 150 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:20000
+    }
+    ,
+    {
+        id:3,
+        title:'Hambuegesa Doble Carne',
+        img:'https://www.shutterstock.com/image-photo/beef-burgers-on-wooden-plate-260nw-1668890842.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Hamburguesa doble carne con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        detailedDescription:'Hamburguesa con 300 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:35000
+    },
+    {
+        id:4,
+        title:'Hambuegesa Ranchera',
+        img:'https://www.shutterstock.com/image-photo/beef-burgers-on-wooden-plate-260nw-1668890842.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Hamburguesa ranchera con reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        detailedDescription:'Hamburguesa con 200 gramos de res, lechuga, tomate, maicitos y queso. Como salsas principales guacamole y salsa de maiz dulce. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:30000
+    },
+    {
+        id:5,
+        title:'Perro Mexicano',
+        img:'https://img.freepik.com/foto-gratis/vista-lateral-salchicha-parrilla-hot-dog-ketchup-queso-cebolla-caramelizada-papas-fritas-mesa_141793-4959.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Perro caliente mexicano, con papas a la francesa y refresco del sabor que prefieras',
+        detailedDescription:'Perro con guacamole, cebolla, jalapeños, champiñones y una lámina de tocino que recubre la salchicha.',
+        price:20000
+    },
+    {
+        id:6,
+        title:'Perro de Arizona',
+        img:'https://img.freepik.com/foto-gratis/vista-lateral-salchicha-parrilla-hot-dog-ketchup-queso-cebolla-caramelizada-papas-fritas-mesa_141793-4959.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Perro caliente con receta de arizona EEUU, con papas a la francesa y refresco del sabor que prefieras',
+        detailedDescription:'Perro con fríjoles, chilli, queso y beicon y una lámina de tocino que recubre la salchica',
+        price:22000
+    },
+    {
+        id:7,
+        title:'Asquerositos',
+        img:'https://img.freepik.com/foto-gratis/vista-lateral-salchicha-parrilla-hot-dog-ketchup-queso-cebolla-caramelizada-papas-fritas-mesa_141793-4959.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Perro caliente con receta venezolana, con papas a la francesa y refresco del sabor que prefieras',
+        detailedDescription:'Perro con salsa barbacoa, salsa de maíz, aguacate, queso rallado, mostaza, mayonesa, salsa de tomate y la cebolla',
+        price:13000
+    },
+    {
+        id:8,
+        title:'Perro Caliente Tradicional',
+        img:'https://img.freepik.com/foto-gratis/vista-lateral-salchicha-parrilla-hot-dog-ketchup-queso-cebolla-caramelizada-papas-fritas-mesa_141793-4959.jpg', //Pendiente para saber donde pondrán las imágenes
+        description:'Perro caliente tradicional con receta de la casa, con papas a la francesa y refresco del sabor que prefieras',
+        detailedDescription:'Perro con salsas al gusto, papas forforito al rededor de la salchicha y cebolla',
+        price:11000
+    }
+]
+
+export default data;
