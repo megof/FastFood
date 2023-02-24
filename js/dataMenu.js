0c9303018fc1a3f83e1aa2a2c63c883ed928c287
Juan Camilo <caamilo9517@gmail.com>
2023-02-24 12:47:52 -0500

    integración Lógica Juan Camilo, maquetación Jasser

--- js/dataMenu.js
+++ js/dataMenu.js
@@ -1,78 +1,90 @@
 //Data de las comidas que tenemos.
-const data=[
+//Data de las comidas que tenemos.
+const dataMenu = [
     {
         id:1111111111,
+        name:'Hambuegesa Mexicana',
         title:'Hamburguesa Mexicana',
         category:'burger',
-        img:'../img/hamburguesa-mexicana.jpg',
-        description:'Hamburguesa auténtica mexicana con los ingredientes frescos y de mejor calidad',
-        detailedDescription:'Hamburguesa con 150 gramos de res, lechuga, tomate, queso y jalapeños. Como salsa principal el guacamole con reseta propia de la casa. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
-        price:25000
+        image:'./assets/1.jpg',
+        shortDescription:'Hamburguesa auténtica mexicana con los ingredientes frescos y de mejor calidad',
+        longDescription:'Hamburguesa con 150 gramos de res, lechuga, tomate, queso y jalapeños. Como salsa principal el guacamole con reseta propia de la casa. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:25000,
+        units:1,
     },
     {
         id:2222222222,
+        name:'Hambuegesa Tradicional',
         title:'HamburguesaTradicional',
         category:'burger',
-        img:'../img/hamburguesa-tradicional.jpg', 
-        description:'Hamburguesa tradicional con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
-        detailedDescription:'Hamburguesa con 150 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
-        price:20000
+        image:'../assets/2.jpg', 
+        shortDescription:'Hamburguesa tradicional con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        longDescription:'Hamburguesa con 150 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:20000,
+        units:1,
     }
     ,
     {
         id:3333333333,
+        name:'Hambuegesa Doble Carne',
         title:'Hamburguesa Doble Carne',
         category:'burger',
-        img:'../img/hamburguesa-doble-carne.jpg', 
-        description:'Hamburguesa doble carne con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
-        detailedDescription:'Hamburguesa con 300 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
-        price:35000
+        image:'../assets/3.jpg', 
+        shortDescription:'Hamburguesa doble carne con la reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        longDescription:'Hamburguesa con 300 gramos de res y cerdo, lechuga, tomate, queso y salsas al gusto. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:35000,
+        units:1,
     },
     {
         id:4444444444,
+        name:'Hambuegesa Ranchera',
         title:'Hamburguesa Ranchera',
         category:'burger',
-        img:'../img/hamburguesa-ranchera.jpg', 
-        description:'Hamburguesa ranchera con reseta de la casa, verduras y queso frescos y de la más alta calidad',
-        detailedDescription:'Hamburguesa con 200 gramos de res, lechuga, tomate, maicitos y queso. Como salsas principales guacamole y salsa de maiz dulce. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
-        price:30000
+        image:'../assets/4.jpg', 
+        shortDescription:'Hamburguesa ranchera con reseta de la casa, verduras y queso frescos y de la más alta calidad',
+        longDescription:'Hamburguesa con 200 gramos de res, lechuga, tomate, maicitos y queso. Como salsas principales guacamole y salsa de maiz dulce. Como acompañante papas a la francesa y un refresco de Cola, todo en uno',
+        price:30000,
+        units:1,
     },
     {
         id:5555555555,
-        title:'Perro Mexicano',
+        name:'Perro Mexicano',
         category:'hotDog',
-        img:'../img/perro-mexicano.jpg', 
-        description:'Perro caliente mexicano, con papas a la francesa y refresco del sabor que prefieras',
-        detailedDescription:'Perro con guacamole, cebolla, jalapeños, champiñones y una lámina de tocino que recubre la salchicha.',
-        price:20000
+        image:'../assets/dog-1.jpg', 
+        shortDescription:'Perro caliente mexicano, con papas a la francesa y refresco del sabor que prefieras',
+        longDescription:'Perro con guacamole, cebolla, jalapeños, champiñones y una lámina de tocino que recubre la salchicha.',
+        price:20000,
+        units:1,
     },
     {
         id:6666666666,
-        title:'Perro de Arizona',
+        name:'Perro de Arizona',
         category:'hotDog',
-        img:'../img/perro-arizona.jpg', 
-        description:'Perro caliente con receta de arizona EEUU, con papas a la francesa y refresco del sabor que prefieras',
-        detailedDescription:'Perro con fríjoles, chilli, queso y beicon y una lámina de tocino que recubre la salchica',
-        price:22000
+        image:'../assets/dog-2.jpg', 
+        shortDescription:'Perro caliente con receta de arizona EEUU, con papas a la francesa y refresco del sabor que prefieras',
+        longDescription:'Perro con fríjoles, chilli, queso y beicon y una lámina de tocino que recubre la salchica',
+        price:22000,
+        units:1,
     },
     {
         id:7777777777,
-        title:'Asquerositos',
+        name:'Asquerositos',
         category:'hotDog',
-        img:'../img/perro-asquerosito.jpg', 
-        description:'Perro caliente con receta venezolana, con papas a la francesa y refresco del sabor que prefieras',
-        detailedDescription:'Perro con salsa barbacoa, salsa de maíz, aguacate, queso rallado, mostaza, mayonesa, salsa de tomate y la cebolla',
-        price:13000
+        image:'../assets/dog-3.jpg', 
+        shortDescription:'Perro caliente con receta venezolana, con papas a la francesa y refresco del sabor que prefieras',
+        longDescription:'Perro con salsa barbacoa, salsa de maíz, aguacate, queso rallado, mostaza, mayonesa, salsa de tomate y la cebolla',
+        price:13000,
+        units:1,
     },
     {
         id:8888888888,
-        title:'Perro Caliente Tradicional',
+        name:'Perro Caliente Tradicional',
         category:'hotDog',
-        img:'../img/perro-comun.jpg', 
-        description:'Perro caliente tradicional con receta de la casa, con papas a la francesa y refresco del sabor que prefieras',
-        detailedDescription:'Perro con salsas al gusto, papas forforito al rededor de la salchicha y cebolla',
-        price:11000
+        image:'../assets/dog-4.jpg', 
+        shortDescription:'Perro caliente tradicional con receta de la casa, con papas a la francesa y refresco del sabor que prefieras',
+        longDescription:'Perro con salsas al gusto, papas forforito al rededor de la salchicha y cebolla',
+        price:11000,
+        units:1,
     }
 ]
-
-export default data;
+export default dataMenu;
--- js/main.js
+++ js/main.js
@@ -1,15 +1,96 @@
 import nav from "./nav.js";
-
+import dataMenu from './dataMenu.js'
 const { createApp } = Vue;
 
 createApp({
-<<<<<<< HEAD
     data() {
         return {
-            
+          dataMenu:[], //Productos que ofrece la tienda
+          numberOfUnits:1, //Cantidad de unidades del producto a pedir.
+          modalInformation:{}, //Información que se muestra en el modal de confirmar el pedido.
+          currentOrder:{}, //Esta es la orden actual, una de las tantas que se mostrará en el carrito de compras.
+          ordersPlaced:[], //EStas son las ordenes que posteriormente se muestran en el carrito de compras
         }
     },
     methods: {
+      increaseUnits(e){
+        //Este es el id que traigo de mi botón.
+        let id=Number(e.target.dataset.id)
+        //Este es el arreglo que debo modificar y su índice
+        let food=this.dataMenu.find(el=>el.id===id)
+        let index=this.dataMenu.map(el=>el.id).indexOf(id)
+        
+         food.units+=1
+         this.dataMenu[index]=food
+         //Actualizo para el que se muestre en el modal el número que colocó el usuario.
+         this.numberOfUnits=food.units
+    },
+    decreaseUnits(e){
+       //Este es el id que traigo de mi botón.
+       let id=Number(e.target.dataset.id)
+       //Este es el arreglo que debo modificar y su índice
+       let food=this.dataMenu.find(el=>el.id===id)
+       let index=this.dataMenu.map(el=>el.id).indexOf(id)
+       if(food.units===1) return
+        food.units-=1
+        this.dataMenu[index]=food
+        //Para el modal
+        this.numberOfUnits=food.units
+    },
+    decreaseUnitsModal(){
+      if(this.numberOfUnits===1) return
+      this.numberOfUnits-=1
+    },
+    increaseUnitsModal(){
+      this.numberOfUnits+=1
+    },
+    deployModal(e){
+        //Actualizo la info que se muestra en el modal de confirmar el pedido
+        this.numberOfUnits=this.dataMenu.find(el=>el.id===Number(e.target.id)).units
+        this.modalInformation=this.dataMenu.find(el=>el.id===Number(e.target.id)) 
+       
+    },
+    closeModal(){
+        const myModalEl = document.getElementById('exampleModal');
+        const modal = bootstrap.Modal.getInstance(myModalEl)
+        modal.hide()
+    },
+    confirmOrder(){
+       
+        this.currentOrder={
+            img:this.modalInformation.image,
+            units:this.numberOfUnits,
+            name:this.modalInformation.name,
+            price:this.modalInformation.price,
+            priceTotal:this.modalInformation.price*this.numberOfUnits
+        }
+        this.ordersPlaced.push(this.currentOrder)
+        localStorage.setItem("orders",JSON.stringify(this.ordersPlaced))
+        this.closeModal()
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Su pedido ha sido realizado, por favor diríjase al carrito de compras para confirmar la compra.',
+            showConfirmButton: true,
+          })
+        
+    }
+
+    },
+    beforeMount(){
+        //Localstorage de los productos que tenemos para ofrecer.
+        if(localStorage.getItem("food")){
+            this.dataMenu=JSON.parse(localStorage.getItem("food"))
+            
+        }else{
+            this.dataMenu=dataMenu
+            localStorage.setItem("food",JSON.stringify(this.dataMenu))
+        }
+       
+        //Localstorage de las ordenes que van para el carrito.
+        if(localStorage.getItem("orders")){
+            this.ordersPlaced=JSON.parse(localStorage.getItem("orders"))
+        }
     },
     mounted() {
         
@@ -19,15 +100,3 @@ createApp({
               
     },
 }).mount("#root");
-=======
-  data() {
-    return {};
-  },
-  methods: {},
-
-  mounted() {
-    nav();
-  },
-  created() {},
-}).mount("#root");
->>>>>>> ee48d15e362ab8410808b9a10ca6fa585a5776b8
